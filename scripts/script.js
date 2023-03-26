d52eb4d2e678ec4fac106cbdef3c9cc6d28d9159
Artur Kulembetov <artur_kulembetov@mail.ru>
2023-03-27 01:41:41 +0500

    fix theme switcher, added SCSS, small fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 // Тень у шапки сайта при скролле
 
-$(window).scroll(function () {
+$(window).scroll(() => {
   var scroll = $(window).scrollTop();
   if (scroll > 0) {
     $("#header").addClass("header__shadow");
@@ -10,32 +10,32 @@ $(window).scroll(function () {
 });
 
 // Константы и переменные
+
+const page = document.querySelector(".page");
 const bikesSelect = document.querySelectorAll(".bikes__links");
 const bikesSurface = document.querySelectorAll(".bikes__surface-link");
-const switcher = document.querySelectorAll(".theme-switcher");
+const switcher = document.querySelector(".footer__input_type_switcher");
 const selector = document.querySelector(".bikes__select");
-const formInput = document.querySelector(".form__input");
-const formButton = document.querySelector(".form__button");
-const formEmail = document.querySelector(".footer__input_type_email");
+const formEmail = document.querySelector("#form-email");
 const popupSubmit = document.querySelector("#popup-submit");
 const buttonClose = document.querySelector(".popup__button-close");
 const slider = document.querySelector(".specification");
 const sliderBox = document.querySelector(".specification__box");
+const sliderButton = document.querySelectorAll(".specification__button");
 const sliderItems = document.querySelectorAll(".specification__item");
 const previousSlide = document.querySelector("#slider-previous");
 const nextSlide = document.querySelector("#slider-next");
-
 // Слайдер
 
 let offset = 0;
 
-function checkButton() {
+const checkButton = () => {
   previousSlide.disabled = offset <= 0;
   nextSlide.disabled =
     offset >= sliderBox.offsetWidth * (sliderItems.length - 1);
-}
+};
 
-function showSlides(previous, next) {
+const showSlides = (previous, next) => {
   next.addEventListener("click", () => {
     offset += sliderBox.offsetWidth;
     sliderBox.style.left = -offset + "px";
@@ -47,15 +47,16 @@ function showSlides(previous, next) {
     sliderBox.style.left = -offset + "px";
     checkButton();
   });
-}
+};
 
 showSlides(previousSlide, nextSlide);
 checkButton();
 
 // Функция переключения слайдов
-function switcherSelector(button, select) {
+
+const switcherSelector = (button, select) => {
   for (let i = 0; i < button.length; i++) {
-    button[i].addEventListener("click", function () {
+    button[i].addEventListener("click", () => {
       for (let j = 0; j < select.length; j++) {
         select[j].classList.remove("bikes__links_opened");
         button[j].classList.remove("bikes__surface-link_active");
@@ -64,11 +65,11 @@ function switcherSelector(button, select) {
       select[i].classList.add("bikes__links_opened");
     });
   }
-}
+};
 
 switcherSelector(bikesSurface, bikesSelect);
 
-function selectItem(selector, select) {
+const selectItem = (selector, select) => {
   selector.addEventListener("change", () => {
     select.forEach((item) => {
       item.classList.remove("bikes__links_opened");
@@ -79,46 +80,53 @@ function selectItem(selector, select) {
       }
     });
   });
-}
+};
 
 selectItem(selector, bikesSelect);
 
 // Открытие и закрытие попапа
 
-function openPopup(popup) {
+const openPopup = (popup) => {
   popup.classList.add("popup_opened");
   document.addEventListener("keydown", closePopupEsc);
   popup.addEventListener("mousedown", closePopupMousedown);
-}
+};
 
-function closePopup(popup) {
+const closePopup = (popup) => {
   popup.classList.remove("popup_opened");
   document.removeEventListener("keydown", closePopupEsc);
   popup.removeEventListener("mousedown", closePopupMousedown);
-}
+};
 
-function closePopupEsc(e) {
+const closePopupEsc = (e) => {
   if (e.key === "Escape") {
     closePopup(document.querySelector(".popup_opened"));
   }
-}
+};
 
-function closePopupMousedown(e) {
+const closePopupMousedown = (e) => {
   if (e.target.classList.contains("popup_opened")) {
     closePopup(e.target);
   }
-}
+};
 
-buttonClose.addEventListener("click", function (e) {
+switcher.addEventListener("click", () => {
+  page.classList.toggle("page_theme_dark");
+  sliderButton.forEach((button) => {
+    button.classList.toggle("specification_button_theme_dark");
+  })
+});
+
+buttonClose.addEventListener("click", (e) => {
   e.preventDefault();
-  formEmail.value = "";
+  formEmail.reset();
   closePopup(popupSubmit);
 });
 
 // Отмена отправки формы
 
-form.addEventListener("submit", function (e) {
+formEmail.addEventListener("submit", (e) => {
   e.preventDefault();
-  formEmail.value = "";
+  formEmail.reset();
   openPopup(popupSubmit);
 });
